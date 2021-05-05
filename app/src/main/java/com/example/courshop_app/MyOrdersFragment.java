15588e114043b4f76b6dcbee4059b9e6f3a7b57e
Patryk Pawłowicz <patryk.pawlowicz@wp.pl>
2021-05-05 21:17:23 +0200

    v.1.0

--- app/src/main/java/com/example/courshop_app/MyOrdersFragment.java
+++ app/src/main/java/com/example/courshop_app/MyOrdersFragment.java
@@ -1,16 +1,44 @@
 package com.example.courshop_app;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.courshop_app.adapters.MyCartAdapter;
+import com.example.courshop_app.models.MyCartModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class MyOrdersFragment extends Fragment {
 
+    FirebaseFirestore db;
+    FirebaseAuth auth;
 
+    TextView totalAmount;
+    RecyclerView recyclerView;
+    MyCartAdapter cartAdapter;
+    List<MyCartModel> cartModelList;
 
     public MyOrdersFragment() {
         // Required empty public constructor
@@ -21,6 +49,47 @@ public class MyOrdersFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_orders, container, false);
+        View root = inflater.inflate(R.layout.fragment_my_orders, container, false);
+
+        db = FirebaseFirestore.getInstance();
+        auth = FirebaseAuth.getInstance();
+        recyclerView = root.findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        totalAmount = root.findViewById(R.id.totalAmount);
+
+        LocalBroadcastManager.getInstance(getActivity())
+                .registerReceiver(mMessageReceiver,new IntentFilter("MyTotalAmount"));
+
+        cartModelList = new ArrayList<>();
+        cartAdapter = new MyCartAdapter(getActivity(),cartModelList);
+
+        recyclerView.setAdapter(cartAdapter);
+
+        db.collection("AddToCart").document(auth.getCurrentUser().getUid())
+                .collection("CurrentUser").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+
+                if (task.isSuccessful()){
+                    for (DocumentSnapshot documentSnapshot : task.getResult().getDocuments()){
+                        MyCartModel cartModel = documentSnapshot.toObject(MyCartModel.class);
+                        cartModelList.add(cartModel);
+                        cartAdapter.notifyDataSetChanged();
+                    }
+                }
+            }
+        });
+
+        return root;
     }
+
+    public BroadcastReceiver mMessageReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            int totalBill = intent.getIntExtra("totalAmount", 0);
+            totalAmount.setText("Total Bill : "+totalBill+"zł");
+
+        }
+    };
 }
---
+++ app/src/main/java/com/example/courshop_app/models/MyCartModel.java
@@ -0,0 +1,70 @@
+package com.example.courshop_app.models;
+
+public class MyCartModel {
+    String courseName;
+    String coursePrice;
+    String currentDate;
+    String currentTime;
+    String totalQuantity;
+    int totalPrice;
+
+    public MyCartModel() {
+    }
+
+    public MyCartModel(String courseName, String coursePrice, String currentDate, String currentTime, String totalQuantity, int totalPrice) {
+        this.courseName = courseName;
+        this.coursePrice = coursePrice;
+        this.currentDate = currentDate;
+        this.currentTime = currentTime;
+        this.totalQuantity = totalQuantity;
+        this.totalPrice = totalPrice;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getCoursePrice() {
+        return coursePrice;
+    }
+
+    public void setCoursePrice(String coursePrice) {
+        this.coursePrice = coursePrice;
+    }
+
+    public String getCurrentDate() {
+        return currentDate;
+    }
+
+    public void setCurrentDate(String currentDate) {
+        this.currentDate = currentDate;
+    }
+
+    public String getCurrentTime() {
+        return currentTime;
+    }
+
+    public void setCurrentTime(String currentTime) {
+        this.currentTime = currentTime;
+    }
+
+    public String getTotalQuantity() {
+        return totalQuantity;
+    }
+
+    public void setTotalQuantity(String totalQuantity) {
+        this.totalQuantity = totalQuantity;
+    }
+
+    public int getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(int totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+}
